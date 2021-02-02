3267f7418b46555e4d84a4f61ead5bc65ecdc605
Evalyn Berleant <eberleant@gmail.com>
2021-02-01 21:51:45 -0600

    Use thumbnails as nodes

--- src/components/Network.jsx
+++ src/components/Network.jsx
@@ -12,16 +12,17 @@ class Network extends React.Component {
   componentDidMount() {
     let dataset = {}
     axios('https://youtaite-network-api.herokuapp.com/collabs')
-      .then(function(response) {
+      .then(response => {
         dataset.nodes = response.data
+        axios('https://youtaite-network-api.herokuapp.com/edges')
+          .then(response => {
+            dataset.edges = response.data
+            this.createNetwork(dataset)
+          })
+          .catch(function(error) {
+            console.log(error)
+          })
       })
-      .then(axios('https://youtaite-network-api.herokuapp.com/edges')
-        .then(function(response) {
-          dataset.edges = response.data
-        })
-        .then(() => {
-          this.createNetwork(dataset)
-        }))
   }
 
   createNetwork(dataset) {
@@ -29,17 +30,15 @@ class Network extends React.Component {
     let h = 600;
     //Initialize a simple force layout, using the nodes and edges in dataset
     var force = d3.forceSimulation(dataset.nodes)
-            .force("charge", d3.forceManyBody())
-            .force("link", d3.forceLink(dataset.edges).id(d => d.id))
-            .force("center", d3.forceCenter().x(w/2).y(h/2));
-
-    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+      .force("charge", d3.forceManyBody())
+      .force("link", d3.forceLink(dataset.edges).id(d => d.id).distance(200))
+      .force("center", d3.forceCenter().x(w/2).y(h/2));
 
     //Create SVG element
     var svg = d3.select("div.network")
-          .append("svg")
-          .attr("width", w)
-          .attr("height", h);
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
     
     //Create edges as lines
     var edges = svg.selectAll("line")
@@ -48,35 +47,58 @@ class Network extends React.Component {
       .append("line")
       .style("stroke", "#ccc")
       .style("stroke-width", 1);
-    
-    //Create nodes as circles
-    var nodes = svg.selectAll("circle")
+
+    //Create nodes
+    // var nodes = svg.selectAll("image")
+    //   .data(dataset.nodes)
+    //   .enter()
+    //   // .append("rect")
+    //   // .attr('width', 250)
+    //   // .attr('height', 145)
+    //   // .attr('stroke', 'red')
+    //   // .attr('fill', 'rgba(0,0,0,0)')
+    //   .append('image')
+    //   .attr('width', 240)
+    //   .attr('height', 135)
+    //   .attr('href', d => d.thumbnail)
+    //   .call(drag(force));
+
+  // Enter any new nodes.
+    let nodes = svg.selectAll('g')
       .data(dataset.nodes)
       .enter()
-      .append("circle")
-      .attr("r", 10)
-      .style("fill", function(d, i) {
-        return colors(i);
+      .append('g')
+      .classed('node', true)
+      .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+      .call(drag(force))
+    nodes.append('rect')
+      .attr('width', 154)
+      .attr('height', 91)
+      .attr('x', -77)
+      .attr('y', -45.5)
+      .attr('stroke', 'firebrick')
+      .attr('fill', 'white')
+      .attr('rx', 10)
+    nodes.append('image')
+      .attr('xlink:href', function(d) { return d.thumbnail;})
+      .attr('height', 81)
+      .attr('x', function() {
+        return -d3.select(this).node().getBBox().width/2
       })
-      .call(drag(force));
-
-    //Add a simple tooltip
-    nodes.append("title")
-       .text(function(d) {
-        return d.yt_id;
-       });
+      .attr('y', function() {
+        return -d3.select(this).node().getBBox().height/2
+      })
     
     //Every time the simulation "ticks", this will be called
     force.on("tick", function() {
-
       edges.attr("x1", function(d) { return d.source.x; })
-         .attr("y1", function(d) { return d.source.y; })
-         .attr("x2", function(d) { return d.target.x; })
-         .attr("y2", function(d) { return d.target.y; });
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
     
-      nodes.attr("cx", function(d) { return d.x; })
-         .attr("cy", function(d) { return d.y; });
-
+      nodes.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')'
+      })
     });
 
     //Define drag event functions
