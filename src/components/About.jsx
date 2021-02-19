1349f2e99830da248615de6770348576c11cbf7c
Evalyn Berleant <eberleant@gmail.com>
2021-02-18 20:24:25 -0600

    Add minimal info to What's this page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,16 @@
 function About() {
   return (<div className='container mt-3'>
     <h2>What's this?</h2>
+    <h3>What is Youtaite Network?</h3>
     <p>Coming soon!~</p>
+    <h3>Report a bug, feature request, data correction, or more</h3>
+    <p>Please use this <a href="https://forms.gle/UGtsik9jtQW448bj8">Google Form</a> to report anything you find!</p>
+    <h3>Contact me</h3>
+    <ul>
+      <li>lyn.youtaite@gmail.com</li>
+      <li><a href="https://twitter.com/lynsings">Twitter</a></li>
+      <li><a href="https://youtube.com/lynsings">Youtube</a></li>
+    </ul>
   </div>);
 }
 
