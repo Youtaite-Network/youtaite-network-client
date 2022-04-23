d209a45403b277d57fbcd6463fde66ad720b3034
Evalyn Berleant <eberleant@gmail.com>
2022-04-23 07:44:40 -0400

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  plugins: [
+    new Dotenv({
+      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+    })
+  ]
+};
